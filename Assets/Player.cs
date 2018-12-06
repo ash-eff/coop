1cba61bb9e267cb2ec268e62abd14aa5e507b471
Ash Fischer <ash.s.fischer@gmail.com>
2018-12-06 10:56:09 -0600

    Networked Player Test

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 input;
+
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        input.z = 0f;
+
+        transform.Translate(input);
+	}
+}
