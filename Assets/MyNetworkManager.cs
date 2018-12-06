6d0130fba592e20a3a9906f5eaa8d7611e3de1a8
Ash Fischer <ash.s.fischer@gmail.com>
2018-12-06 09:24:49 -0600

    TEST host/client networking

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad + "Starting Host.");
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Host Started.");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Client start requested.");
+        //base.OnStartClient(myClient);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + "Client Connected to IP: " + conn.address);
+        //base.OnClientConnect(conn);
+    }
+}
